fe2bd0a31257a561088b0ce110066afca9f17189
tosterberg <Osterberg614@gmail.com>
2021-03-24 18:16:46 -0500

    Updated navbar.component to allow logout and clear local storage

--- open-sponsor/src/app/components/navbar/navbar.component.ts
+++ open-sponsor/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from './../../services/validate.service';
+import { AuthService } from './../../services/auth.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+      private validateService: ValidateService,
+      private flashMessage: FlashMessagesService,
+      private authService: AuthService,
+      private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onLogoutClick(){
+      this.authService.logout();
+      this.flashMessage.show("Logout Successful", {
+          cssClass: 'alert-success',
+          timeout: 3000});
+      this.router.navigate(['/login']);
+      return false;
   }
 
 }
--- open-sponsor/src/app/services/auth.service.ts
+++ open-sponsor/src/app/services/auth.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+
 @Injectable()
 export class AuthService {
     authToken: any;
     user: any;
 
-    constructor(private http: HttpClient) { }
+    constructor(
+        private http: HttpClient,
+    ) { }
 
     registerUser(user){
         let headers = new HttpHeaders();
@@ -14,5 +17,26 @@ export class AuthService {
         headers.append('Content-Type', 'application/json');
 
         return this.http.post('http://localhost:3000/users/register', user, {headers: headers});
+    }
+
+    authenticateUser(user){
+        let headers = new HttpHeaders();
+
+        headers.append('Content-Type', 'application/json');
+
+        return this.http.post('http://localhost:3000/users/authenticate', user, {headers: headers});
+    }
+
+    storeUserData(token, user){
+        localStorage.setItem('id_token', token);
+        localStorage.setItem('user', JSON.stringify(user));
+        this.authToken = token;
+        this.user = user;
+    }
+
+    logout(){
+        this.authToken = null;
+        this.user = null;
+        localStorage.clear();
     }
 }
